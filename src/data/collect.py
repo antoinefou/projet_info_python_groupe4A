049d5096958655379424f8b2354a25d1d363eb88
melloul <yassine.melloul@eleve.ensai.fr>
2026-04-18 13:21:40 +0000

    modifs

--- src/data/collect.py
+++ src/data/collect.py
@@ -1,12 +1,10 @@
 """ Collecte de données via url ou api"""
 import pandas as pd
 import requests
-import zipfile
-from io import BytesIO, TextIOWrapper
 import json
 
 
-url_dvf = "https://www.data.gouv.fr/api/1/datasets/r/99a26050-b94f-4ffc-9eb0-73ed28a895d1"
+url_dvf = "https://static.data.gouv.fr/resources/demandes-de-valeurs-foncieres/20260405-002321/valeursfoncieres-2025.txt.zip"
 
 api_dvf = "https://valoris-immo.fr/api/v1/prix-median"
 
@@ -36,76 +34,10 @@ def load__data_url_zip_txt(url: str) -> pd.DataFrame:
     pd.DataFrame
         DataFrame contenant les données DVF décompressées et chargées.
 
-    Process
-    -------
-    1. Téléchargement du fichier depuis l'URL
-    2. Décompression du fichier ZIP en mémoire
-    3. Extraction du fichier texte contenu dans le ZIP
-    4. Lecture du fichier en DataFrame pandas
-    """
-    # 1. download
-    response = requests.get(url)
-    response.raise_for_status()
-
-    # 2. unzip
-    with zipfile.ZipFile(BytesIO(response.content)) as z:
-        file_name = z.namelist()[0]  # premier fichier dans le zip
-
-        with z.open(file_name) as f:
-            # 3. lecture texte avec bon encodage
-            df = pd.read_csv(
-                f, encoding="utf-8",
-                sep="|",
-                low_memory=False
-            )
-
-    return df
-
-
-def load_data_url_zip_csv(url: str) -> pd.DataFrame:
-    """
-    Télécharge et charge un fichier au format .zip contenant un CSV.
-
-    Le fichier est compressé et contient un CSV structuré.
-
-    Parameters
-    ----------
-    url : str
-        URL pointant vers un fichier compressé (.zip contenant un CSV)
-
-    Returns
-    -------
-    pd.DataFrame
-        DataFrame pandas contenant les données DVF décompressées.
-
-    Process
-    -------
-    1. Téléchargement du fichier ZIP depuis l'URL
-    2. Ouverture du ZIP en mémoire (sans extraction disque)
-    3. Lecture du fichier CSV contenu dans l'archive
-    4. Retour du DataFrame pandas
     """
 
-    # 1. Téléchargement du fichier ZIP
-    response = requests.get(url)
-    response.raise_for_status()
+    df = pd.read_csv(url, sep="|", compression="zip")
 
-    # 2. Ouverture du ZIP en mémoire
-    with zipfile.ZipFile(BytesIO(response.content)) as z:
-
-        # récupération du premier fichier dans l’archive
-        file_name = z.namelist()[0]
-
-        # 3. Lecture du CSV
-        with z.open(file_name) as f:
-
-            df = pd.read_csv(
-                TextIOWrapper(f, encoding="utf-8"),  # adapter en "latin-1" si besoin
-                sep=";",                              # CSV ;
-                low_memory=False
-            )
-
-    # 4. Retour du DataFrame
     return df
 
 
@@ -113,7 +45,6 @@ url_api = "https://api.insee.fr/melodi/data/DS_ESTIMATION_POPULATION?TIME_PERIOD
 
 
 def load_data_api(api_url):
-
     get_data = requests.get(api_url, verify=False)
     data_from_net = get_data.content
     data = json.loads(data_from_net)
